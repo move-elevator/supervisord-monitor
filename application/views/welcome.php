45c8e026f9bad0e5d5809b1cb9fadc4c34b6c65d
Sebastian Kobisch <sko@move-elevator.de>
2015-06-03 10:36:45 +0200

    Update welcome

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -54,12 +54,12 @@ $muted = (isset($_COOKIE['mute'])?$_COOKIE['mute']:0);
     </div>
 	
 
-	<div class="container">
+	<div class="container" style="margin-top: 40px">
 	
 		<?php
 		if($muted){
 			echo '<div class="row"><div class="span4 offset4 label label-important" style="padding:10px;margin-bottom:20px;text-align:center;">';
-			echo 'Sound muted for '.timespan(time(),$muted).' <span class="pull-right"><a href="?mute=0" style="color:white;"><i class="icon-music icon-white"></i> Unmute</a></span></div></div>';
+			echo 'Sound muted for '.timespan(time(),$muted).' <span class="text-center"><a href="?mute=0" style="color:white;"><i class="icon-music icon-white"></i> Unmute</a></span></div></div>';
 		}
 	
 		?>
@@ -75,12 +75,11 @@ $muted = (isset($_COOKIE['mute'])?$_COOKIE['mute']:0);
                     }
                     $ui_url = $base_url . $parsed_url['host'] . ':' . $cfg[$name]['port']. '/';
 				?>
-				<div class="span<?php echo ($this->config->item('supervisor_cols')==2?'6':'4');?>">
 				<table class="table table-bordered table-condensed table-striped">
 					<tr><th colspan="4">
 						<a href="<?php echo $ui_url; ?>"><?php echo $name; ?></a> <?php if($this->config->item('show_host')){ ?><i><?php echo $parsed_url['host']; ?></i><?php } ?>
 						<?php if(isset($cfg[$name]['username'])){echo '<i class="icon-lock icon-green" style="color:blue" title="Authenticated server connection"></i>';}?>
-						<span class="server-btns pull-right">
+						<span class="server-btns text-center">
 							<a href="/control/stopall/<?php echo $name; ?>" class="btn btn-mini btn-inverse" type="button"><i class="icon-stop icon-white"></i> Stop all</a>
 							<a href="/control/startall/<?php echo $name; ?>" class="btn btn-mini btn-success" type="button"><i class="icon-play icon-white"></i> Start all</a>
 							<a href="/control/restartall/<?php echo $name; ?>" class="btn btn-mini btn-primary" type="button"><i class="icon icon-refresh icon-white"></i> Restart all</a>
@@ -99,10 +98,12 @@ $muted = (isset($_COOKIE['mute'])?$_COOKIE['mute']:0);
 						if(!is_array($item)){ echo '<tr><td colspan="4">'.$item.'</td></tr>';continue;}
 						$pid = $uptime = '&nbsp;';
 						$status = $item['statename'];
+
 						if($status=='RUNNING'){
 							$class = 'success';
 							list($pid,$uptime) = explode(",",$item['description']);
 						}
+
 						elseif($status=='STARTING') $class = 'info';
 						elseif($status=='FATAL') $class = 'important';
 						elseif($status=='STOPPED') $class = 'inverse';
@@ -110,19 +111,20 @@ $muted = (isset($_COOKIE['mute'])?$_COOKIE['mute']:0);
 
 						$uptime = str_replace("uptime ","",$uptime);
 						?>
-						<tr>
-							<td><?php
+
+						<tr class="<?php if($status === 'FATAL') { echo 'alert-tr'; } else { echo 'normal-tr'; } ?>">
+							<td width="50%" style="width: 50%;"><?php
 								echo $item_name;
 								if($check){
 									$alert = true;
-		echo '<span class="pull-right"><a href="/control/clear/'.$name.'/'.$item_name.'" id="'.$name.'_'.$item_name.'" onclick="return false" data-toggle="popover" data-message="'.htmlspecialchars($check).
+		echo '<span class="text-center"><a href="/control/clear/'.$name.'/'.$item_name.'" id="'.$name.'_'.$item_name.'" onclick="return false" data-toggle="popover" data-message="'.htmlspecialchars($check).
 			'" data-original-title="'.$item_name.'@'.$name.'" class="pop btn btn-mini btn-danger"><img src="/img/alert_icon.png" /></a></span>';
 								}
 								?>
 							</td>
-							<td width="10"><span class="label label-<?php echo $class;?>"><?php echo $status;?></span></td>
-							<td width="80" style="text-align:right"><?php echo $uptime;?></td>
-							<td style="width:1%">
+							<td width="20%" style="text-align: center; width: 20%;"><span class="label label-<?php echo $class;?>"><?php echo $status;?></span></td>
+							<td width="10%" style="text-align: center; width: 10%;"><?php echo $uptime;?></td>
+							<td width="20%" style="text-align: center; width: 20%;">
 								<!--div class="btn-group">
 									<button class="btn btn-mini">Action</button>
 									<button class="btn btn-mini dropdown-toggle" data-toggle="dropdown">
@@ -134,9 +136,9 @@ $muted = (isset($_COOKIE['mute'])?$_COOKIE['mute']:0);
 									</ul>
 								</div//-->
 								<?php if($status=='RUNNING'){ ?>
-								<a href="/control/stop/<?php echo $name.'/'.$item_name;?>" class="btn btn-mini btn-inverse" type="button"><i class="icon-stop icon-white"></i></a>
+								<a href="/control/stop/<?php echo $name.'/'.$item_name;?>" class="btn btn-mini btn-inverse text-center" type="button"><i class="icon-stop icon-white"></i></a>
 								<?php } if($status=='STOPPED' || $status == 'EXITED'){ ?>
-								<a href="/control/start/<?php echo $name.'/'.$item_name;?>" class="btn btn-mini btn-success" type="button"><i class="icon-play icon-white"></i></a>
+								<a href="/control/start/<?php echo $name.'/'.$item_name;?>" class="btn btn-mini btn-success text-center" type="button"><i class="icon-play icon-white"></i></a>
 								<?php } ?>
 							</td>
 						</tr>
@@ -148,7 +150,10 @@ $muted = (isset($_COOKIE['mute'])?$_COOKIE['mute']:0);
 			</div>
 				<?php
 				}
-				if($alert && !$muted && $this->config->item('enable_alarm')){
+                echo $this->config->item('enable_alarm');
+               # echo '<div class="omg-alarm"></div>';
+
+                if($status === 'FATAL' && !$muted && $this->config->item('enable_alarm')){
 					echo '<embed height="0" width="0" src="/sounds/alert.mp3">';
 				}
 				if($alert){
@@ -158,7 +163,6 @@ $muted = (isset($_COOKIE['mute'])?$_COOKIE['mute']:0);
 				}
 				
 				?>
-		</div>
 	</div>
 
     </div> <!-- /container -->
@@ -179,7 +183,7 @@ $muted = (isset($_COOKIE['mute'])?$_COOKIE['mute']:0);
 		$content+= '<input type="hidden" name="issue[assigned_to_id]" value="<?php echo $this->config->item('redmine_assigne_id');?>"/>';
 		$content+= '<input type="submit" class="btn btn-small btn-inverse" value="Start New Ticket"/>';
 		$content+= '</form></div>';
-		$content+= '<div class="pull-right"><a href="#" onclick="$(\'#'+$(this).attr('id')+'\').popover(\'hide\');startTimer();" class="btn btn-small btn-primary">ok</a>&nbsp;&nbsp;';
+		$content+= '<div class="text-center"><a href="#" onclick="$(\'#'+$(this).attr('id')+'\').popover(\'hide\');startTimer();" class="btn btn-small btn-primary">ok</a>&nbsp;&nbsp;';
 		$content+= '<a href="'+$(this).attr('href')+'" class="btn btn-small btn-danger">Clear</a> &nbsp; </div>';
 		return $content;
 	}
